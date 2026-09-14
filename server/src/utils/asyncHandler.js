543ee80fdbe122ffb063336a8021128743007c7a
Aneesh Mathew <984271+aneeshmathew@users.noreply.github.com>
2026-09-14 14:28:17 -0700

    cleanup old code

--- server/src/utils/asyncHandler.js
+++
@@ -1,9 +0,0 @@
-// Wraps an Express route handler: (req, res, next) => Promise
-// so any rejected promise / thrown error is forwarded to next() automatically.
-function asyncHandler(fn) {
-  return function wrapped(req, res, next) {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-}
-
-module.exports = asyncHandler;
